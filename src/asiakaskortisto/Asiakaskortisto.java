14afc59d40383dbfacaf406ecbb793037b92d211
Kati Kyllonen <kati.a.kyllonen@helsinki.fi>
2012-01-22 17:19:15 +0200

    dokumentti

--- src/asiakaskortisto/Asiakaskortisto.java
+++ src/asiakaskortisto/Asiakaskortisto.java
@@ -14,6 +14,8 @@ public class Asiakaskortisto {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Valikko");
+        
     }
 
 }
